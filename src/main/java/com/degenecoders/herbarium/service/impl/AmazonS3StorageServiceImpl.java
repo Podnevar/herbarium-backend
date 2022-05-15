631b451d9dfcd8c99fc951df24840774a1460385
Rene Podnevar <rene.podnevar97@gmail.com>
2022-05-15 03:34:49 +0200

    Configure the HTTP Port.

--- src/main/java/com/degenecoders/herbarium/service/impl/AmazonS3StorageServiceImpl.java
+++ src/main/java/com/degenecoders/herbarium/service/impl/AmazonS3StorageServiceImpl.java
@@ -19,29 +19,11 @@ public class AmazonS3StorageServiceImpl implements StorageService {
 
     @Override
     public void store(String fileName, byte[] bytes) {
-//        if (formData.filename == null || formData.filename.isEmpty()) {
-//            return Response.status(Status.BAD_REQUEST).build();
-//        }
-//
-//        if (formData.mimetype == null || formData.mimetype.isEmpty()) {
-//            return Response.status(Status.BAD_REQUEST).build();
-//        }
         s3.listBuckets();
         PutObjectRequest putObjectRequest = PutObjectRequest.builder()
                 .bucket(BUCKET_NAME)
                 .key(fileName)
                 .build();
         PutObjectResponse putResponse = s3.putObject(putObjectRequest,RequestBody.fromBytes(bytes));
-//        if (putResponse != null) {
-//            return Response.ok().status(Status.CREATED).build();
-//        } else {
-//            return Response.serverError().build();
-//        }
     }
-
-//    public Response downloadFile(String objectKey) {
-//        S3Object s3object = s3.getObject(BUCKET_NAME, "picture/pic.png");
-//        S3ObjectInputStream inputStream = s3object.get;
-//        FileUtils.copyInputStreamToFile(inputStream, new File("/Users/user/Desktop/hello.txt"));
-//    }
 }
